41f3b025fb34a48856b8b57a4cf0e5137a1349f5
David Britton <davidpbritton@gmail.com>
2018-05-10 16:50:34 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+repo to help in repro of: https://forum.snapcraft.io/t/localization-of-python-snaps/2066
---
+++ lcall_bug/__main__.py
@@ -0,0 +1,5 @@
+import sys
+import lcall_bug
+
+if __name__ == '__main__':
+    sys.exit(lcall_bug.main())
